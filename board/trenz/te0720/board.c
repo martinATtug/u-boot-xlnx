8e602801f2546441230bcdae5d4add8d0fdee7ec
kienko <a.kienko@gmail.com>
2013-06-09 07:14:09 -0700

    Added board configuration for TE0720-01

---
+++ board/trenz/te0720/board.c
@@ -0,0 +1,239 @@
+/*
+-- Copyright (C) 2013 Trenz Electronic GmbH
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a 
+-- copy of this software and associated documentation files (the "Software"), 
+-- to deal in the Software without restriction, including without limitation 
+-- the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+-- and/or sell copies of the Software, and to permit persons to whom the 
+-- Software is furnished to do so, subject to the following conditions:
+-- 
+-- The above copyright notice and this permission notice shall be included 
+-- in all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+-- OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+-- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+-- IN THE SOFTWARE.
+ */
+
+#include <common.h>
+#include <command.h>
+#include <asm/io.h>
+#include <asm/arch/mmc.h>
+#include <asm/arch/nand.h>
+#include <netdev.h>
+#include <zynqpl.h>
+#include <asm/arch/sys_proto.h>
+#include <miiphy.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+/* Bootmode setting values */
+#define BOOT_MODES_MASK		0x0000000F
+#define QSPI_MODE		0x00000001
+#define NOR_FLASH_MODE		0x00000002
+#define NAND_FLASH_MODE		0x00000004
+#define SD_MODE			0x00000005
+#define JTAG_MODE		0x00000000
+
+#ifdef CONFIG_FPGA
+Xilinx_desc fpga;
+
+/* It can be done differently */
+Xilinx_desc fpga010 = XILINX_XC7Z010_DESC(0x10);
+Xilinx_desc fpga020 = XILINX_XC7Z020_DESC(0x20);
+Xilinx_desc fpga030 = XILINX_XC7Z030_DESC(0x30);
+Xilinx_desc fpga045 = XILINX_XC7Z045_DESC(0x45);
+#endif
+
+int board_init(void)
+{
+#ifdef CONFIG_FPGA
+	u32 idcode;
+
+	
+	idcode = zynq_slcr_get_idcode();
+
+	switch (idcode) {
+	case XILINX_ZYNQ_7010:
+		fpga = fpga010;
+		break;
+	case XILINX_ZYNQ_7020:
+		fpga = fpga020;
+		break;
+	case XILINX_ZYNQ_7030:
+		fpga = fpga030;
+		break;
+	case XILINX_ZYNQ_7045:
+		fpga = fpga045;
+		break;
+	}
+#endif
+
+	/* temporary hack to clear pending irqs before Linux as it
+	 * will hang Linux
+	 */
+	writel(0x26d, 0xe0001014);
+
+	/* temporary hack to take USB out of reset til the is fixed
+	 * in Linux
+	 */
+	writel(0x80, 0xe000a204);
+	writel(0x80, 0xe000a208);
+	writel(0x80, 0xe000a040);
+	writel(0x00, 0xe000a040);
+	writel(0x80, 0xe000a040);
+
+	icache_enable();
+
+#ifdef CONFIG_FPGA
+	fpga_init();
+	fpga_add(fpga_xilinx, &fpga);
+#endif
+
+	return 0;
+}
+
+char bin2char(unsigned char c)
+{
+	if((c >= 0) && (c <= 9)){
+		return '0' + c;
+	}
+	return 'A' + c - 10;
+}
+
+int board_late_init(void)
+{
+	// MII patch
+	*((uint *)0xE000B000) = 0x00000010;
+
+	switch ((zynq_slcr_get_boot_mode()) & BOOT_MODES_MASK) {
+	case QSPI_MODE:
+		setenv("modeboot", "qspiboot");
+		break;
+	case SD_MODE:
+		setenv("modeboot", "sdboot");
+		break;
+	default:
+		setenv("modeboot", "");
+		break;
+	}
+	return 0;
+}
+
+#ifdef CONFIG_CMD_NET
+int board_eth_init(bd_t *bis)
+{
+	unsigned char	i;
+	unsigned char	mac[6];
+	unsigned short	data;
+	unsigned int	model;
+	char		*devname;
+	char ethaddr[] = "00:0a:35:00:01:32";
+	char eui48[] = "[00 0A 35 00 01 32]";
+	char board[] = "TE0000-00";
+	char sc_ver[] = "0.00";
+
+	u32 ret = 0;
+
+#ifdef CONFIG_XILINX_AXIEMAC
+	ret |= xilinx_axiemac_initialize(bis, XILINX_AXIEMAC_BASEADDR,
+						XILINX_AXIDMA_BASEADDR);
+#endif
+#ifdef CONFIG_XILINX_EMACLITE
+	u32 txpp = 0;
+	u32 rxpp = 0;
+# ifdef CONFIG_XILINX_EMACLITE_TX_PING_PONG
+	txpp = 1;
+# endif
+# ifdef CONFIG_XILINX_EMACLITE_RX_PING_PONG
+	rxpp = 1;
+# endif
+	ret |= xilinx_emaclite_initialize(bis, XILINX_EMACLITE_BASEADDR,
+			txpp, rxpp);
+#endif
+
+#if defined(CONFIG_ZYNQ_GEM)
+# if defined(CONFIG_ZYNQ_GEM_BASEADDR0)
+	ret |= zynq_gem_initialize(bis, CONFIG_ZYNQ_GEM_BASEADDR0);
+# endif
+# if defined(CONFIG_ZYNQ_GEM_BASEADDR1)
+	ret |= zynq_gem_initialize(bis, CONFIG_ZYNQ_GEM_BASEADDR1);
+# endif
+#endif
+	/*Configure MAC address*/
+	/*mii read 1a 9-b*/
+#if defined(CINFIG_MII_INIT)
+	mii_init();
+#endif
+	devname = miiphy_get_current_dev();
+	miiphy_read(devname, 0x1A, 0x09, &data);
+	mac[0] = (unsigned char)(data >> 8);
+	mac[1] = (unsigned char)(data & 0xFF);
+	miiphy_read(devname, 0x1A, 0x0A, &data);
+	mac[2] = (unsigned char)(data >> 8);
+	mac[3] = (unsigned char)(data & 0xFF);
+	miiphy_read(devname, 0x1A, 0x0B, &data);
+	mac[4] = (unsigned char)(data >> 8);
+	mac[5] = (unsigned char)(data & 0xFF);
+	
+	for(i = 0; i < 6; i++){
+		ethaddr[i*3 + 0] = bin2char(mac[i] >> 4);
+		ethaddr[i*3 + 1] = bin2char(mac[i] & 0x0F);
+		eui48[i*3 + 1] =  bin2char(mac[i] >> 4);
+		eui48[i*3 + 2] =  bin2char(mac[i] & 0x0F);
+	}
+	setenv("ethaddr", ethaddr);
+	setenv("eui48", eui48);
+/*Configure board information*/
+	miiphy_read(devname, 0x1A, 0x02, &data);
+	model = data << 6;
+	miiphy_read(devname, 0x1A, 0x03, &data);
+	model |= (data >> 10) & 0x3F;
+
+	board[3] = '0' + ((model >> 12) & 0xF); //7
+	board[4] = '0' + ((model >> 8) & 0xF);  //2
+	board[5] = '0' + ((model >> 4) & 0xF);  //0-0
+
+	board[8] = '0' + (model & 0xF);
+
+	setenv("board", board);
+
+	sc_ver[0] = '0' + (data & 0xF);
+//	sc_ver[2] = ???; TODO minor version bits!
+	
+	setenv("scver", sc_ver);
+	
+	return ret;
+}
+#endif
+
+#ifdef CONFIG_CMD_MMC
+int board_mmc_init(bd_t *bd)
+{
+	// MMC Init run before SF init - we can put SF patch here
+	if(((zynq_slcr_get_boot_mode()) & BOOT_MODES_MASK) == SD_MODE){
+		// Little patch for SD mode
+		*((uint *)0xE000D000) = 0x800AFCD7;
+	}
+	return zynq_mmc_init(bd);
+}
+#endif
+
+#ifdef CONFIG_CMD_NAND
+int board_nand_init(struct nand_chip *nand_chip)
+{
+	return zynq_nand_init(nand_chip);
+}
+#endif
+
+int dram_init(void)
+{
+	gd->ram_size = CONFIG_SYS_SDRAM_SIZE;
+
+	return 0;
+}
--- include/configs/zynq_gigazee.h
+++ include/configs/zynq_gigazee.h
@@ -1,17 +1,23 @@
 /*
- * Trenz Electronics 
- *
- * Configuration for Zynq Evaluation and Development Board - TE0720
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- * MA 02111-1307 USA
+-- Copyright (C) 2013 Trenz Electronic GmbH
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a 
+-- copy of this software and associated documentation files (the "Software"), 
+-- to deal in the Software without restriction, including without limitation 
+-- the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+-- and/or sell copies of the Software, and to permit persons to whom the 
+-- Software is furnished to do so, subject to the following conditions:
+-- 
+-- The above copyright notice and this permission notice shall be included 
+-- in all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+-- OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+-- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+-- IN THE SOFTWARE.
  */
 
 #ifndef __CONFIG_ZYNQ_GIGAZEE_H
@@ -29,105 +35,317 @@
 #define CONFIG_ZYNQ_SPI
 #define CONFIG_ZYNQ_I2C
 
-#include <configs/zynq_common.h>
+/* High Level Configuration Options */
+#define CONFIG_ARMV7 /* CPU */
+#define CONFIG_ZYNQ /* SoC */
 
-#undef CONFIG_IPADDR
-#define CONFIG_IPADDR   192.168.42.50
-#undef CONFIG_SERVERIP
-#define CONFIG_SERVERIP 192.168.42.2
-#undef SD_BASEADDR
-#define SD_BASEADDR     0xE0101000
-#undef CONFIG_ZYNQ_I2C_CTLR_0
-#define CONFIG_ZYNQ_I2C_CTLR_1
+/* Default environment */
+#define CONFIG_IPADDR   	192.168.42.50
+#define CONFIG_SERVERIP 	192.168.42.2
+
+#define CONFIG_SYS_SDRAM_BASE	0
+#define CONFIG_SYS_SDRAM_SIZE	PHYS_SDRAM_1_SIZE
+
+/* Total Size of Environment Sector */
+#define CONFIG_ENV_SIZE			(128 << 10)
+
+/* allow to overwrite serial and ethaddr */
+#define CONFIG_ENV_OVERWRITE
+
+/* Size of malloc() pool */
+#define CONFIG_SYS_MALLOC_LEN		0x400000
+
+/* Serial drivers */
+#define CONFIG_BAUDRATE			115200
+#define CONFIG_SYS_BAUDRATE_TABLE	{ 9600, 38400, 115200 }
+
+/* Zynq serial driver */
+#ifdef CONFIG_ZYNQ_SERIAL_UART0
+# define CONFIG_ZYNQ_SERIAL_BASEADDR0	0xE0000000
+# define CONFIG_ZYNQ_SERIAL_BAUDRATE0	CONFIG_BAUDRATE
+# define CONFIG_ZYNQ_SERIAL_CLOCK0	50000000
+#endif
+
+#ifdef CONFIG_ZYNQ_SERIAL_UART1
+# define CONFIG_ZYNQ_SERIAL_BASEADDR1	0xE0001000
+# define CONFIG_ZYNQ_SERIAL_BAUDRATE1	CONFIG_BAUDRATE
+# define CONFIG_ZYNQ_SERIAL_CLOCK1	50000000
+#endif
+
+#if defined(CONFIG_ZYNQ_SERIAL_UART0) || defined(CONFIG_ZYNQ_SERIAL_UART1)
+#define CONFIG_ZYNQ_SERIAL
+#endif
+
+/* Ethernet driver */
+#ifdef CONFIG_ZYNQ_GEM0
+# define CONFIG_ZYNQ_GEM_BASEADDR0	0xE000B000
+#endif
+
+#ifdef CONFIG_ZYNQ_GEM1
+# define CONFIG_ZYNQ_GEM_BASEADDR1	0xE000C000
+#endif
+
+#if defined(CONFIG_ZYNQ_GEM0) || defined(CONFIG_ZYNQ_GEM1)
+# define CONFIG_NET_MULTI
+# define CONFIG_ZYNQ_GEM
+# define CONFIG_MII
+# define CONFIG_SYS_FAULT_ECHO_LINK_DOWN
+# define CONFIG_PHYLIB
+# define CONFIG_PHY_MARVELL
+# define CONFIG_SYS_ENET
+#endif
+
+/* SCU timer address is hardcoded */
+#define CONFIG_SCUTIMER_BASEADDR	0xF8F00600
+#ifndef CONFIG_CPU_FREQ_HZ
+#define CONFIG_CPU_FREQ_HZ		800000000
+#endif
+#define CONFIG_SYS_HZ			1000
+
+/* Miscellaneous configurable options */
+#define CONFIG_SYS_PROMPT		"zynq-uboot> "
+#define CONFIG_SYS_HUSH_PARSER	/* use "hush" command parser */
+#define CONFIG_SYS_PROMPT_HUSH_PS2	"> "
+
+#define CONFIG_CMDLINE_EDITING
+#define CONFIG_AUTO_COMPLETE
+#define CONFIG_SYS_LONGHELP
+#define CONFIG_BOARD_LATE_INIT
+#define CONFIG_SYS_MAXARGS		16
+#define CONFIG_SYS_CBSIZE		2048
+#define CONFIG_SYS_PBSIZE		(CONFIG_SYS_CBSIZE + \
+					sizeof(CONFIG_SYS_PROMPT) + 16)
+
+/* Open Firmware flat tree */
+#define CONFIG_OF_LIBFDT
+
+#include <config_cmd_default.h>
+
+#ifdef CONFIG_SYS_ENET
+# define CONFIG_CMD_PING
+# define CONFIG_CMD_MII
+#else
+# undef CONFIG_CMD_NET
+# undef CONFIG_CMD_NFS
+#endif
+
+/* NOR */
+#ifndef CONFIG_SYS_NO_FLASH
+# define CONFIG_SYS_FLASH_BASE		0xE2000000
+# define CONFIG_SYS_FLASH_SIZE		(16 * 1024 * 1024)
+# define CONFIG_SYS_MAX_FLASH_BANKS	1
+/* max number of sectors/blocks on one chip */
+# define CONFIG_SYS_MAX_FLASH_SECT	512
+# define CONFIG_SYS_FLASH_ERASE_TOUT	1000
+# define CONFIG_SYS_FLASH_WRITE_TOUT	5000
+# define CONFIG_FLASH_SHOW_PROGRESS	10
+# define CONFIG_SYS_FLASH_CFI
+# undef CONFIG_SYS_FLASH_EMPTY_INFO
+# define CONFIG_FLASH_CFI_DRIVER
+# undef CONFIG_SYS_FLASH_PROTECTION /* don't use hardware protection */
+/* use buffered writes (20x faster) */
+# define CONFIG_SYS_FLASH_USE_BUFFER_WRITE
+#endif
+
+/* QSPI */
+#ifdef CONFIG_ZYNQ_SPI
+# define CONFIG_SF_DEFAULT_SPEED	30000000
+# define CONFIG_SPI_FLASH
+# define CONFIG_SPI_FLASH_SPANSION
+# define CONFIG_SPI_FLASH_STMICRO
+# define CONFIG_SPI_FLASH_WINBOND
+# define CONFIG_CMD_SPI
+# define CONFIG_CMD_SF
+#endif
+
+/* MMC */
+#ifdef CONFIG_MMC
+# define CONFIG_GENERIC_MMC
+# define CONFIG_SDHCI
+# define CONFIG_ZYNQ_SDHCI
+# define CONFIG_CMD_MMC
+# define CONFIG_CMD_FAT
+# define CONFIG_SUPPORT_VFAT
+# define CONFIG_CMD_EXT2
+# define CONFIG_DOS_PARTITION
+#endif
+
+/* NAND */
+#ifdef CONFIG_NAND_ZYNQ
+# define CONFIG_CMD_NAND
+# define CONFIG_CMD_NAND_LOCK_UNLOCK
+# define CONFIG_SYS_MAX_NAND_DEVICE 1
+# define CONFIG_SYS_NAND_BASE XPSS_NAND_BASEADDR
+# define CONFIG_SYS_NAND_ONFI_DETECTION
+# define CONFIG_MTD_DEVICE
+#endif
+
+/* I2C */
+#ifdef CONFIG_ZYNQ_I2C
+# define CONFIG_CMD_I2C
+# define CONFIG_ZYNQ_I2C_CTLR_0
+# define CONFIG_HARD_I2C		1
+# define CONFIG_SYS_I2C_SPEED		100000
+# define CONFIG_SYS_I2C_SLAVE		1
+# define CONFIG_ZYNQ_I2C_CTLR_1
+#endif
+
+/* EEPROM */
+#ifdef CONFIG_ZYNQ_EEPROM
+# define CONFIG_CMD_EEPROM
+# define CONFIG_SYS_I2C_EEPROM_ADDR_LEN		1
+# define CONFIG_SYS_I2C_EEPROM_ADDR		0x54
+# define CONFIG_SYS_EEPROM_PAGE_WRITE_BITS	4
+# define CONFIG_SYS_EEPROM_PAGE_WRITE_DELAY_MS	5
+# define CONFIG_SYS_EEPROM_SIZE			1024 /* Bytes */
+#endif
+
+#ifndef CONFIG_ENV_IS_NOWHERE
+# ifndef CONFIG_SYS_NO_FLASH
+/* Environment in NOR flash */
+#  define CONFIG_ENV_IS_IN_FLASH
+# elif defined(CONFIG_ZYNQ_SPI)
+/* Environment in Serial Flash */
+#  define CONFIG_ENV_IS_IN_SPI_FLASH
+# elif defined(CONFIG_NAND_ZYNQ)
+/* Environment in NAND flash */
+#  define CONFIG_ENV_IS_IN_NAND
+# elif defined(CONFIG_SYS_NO_FLASH)
+#  define CONFIG_ENV_IS_NOWHERE
+# endif
+
+# define CONFIG_ENV_SECT_SIZE		CONFIG_ENV_SIZE
+# define CONFIG_ENV_OFFSET		0xF50000
+# define CONFIG_CMD_SAVEENV	/* Command to save ENV to Flash */
+#endif
+
+/* For development/debugging */
+#ifdef DEBUG
+# define CONFIG_CMD_REGINFO
+# define CONFIG_PANIC_HANG
+#endif
+
+
+/* default boot is according to the bootmode switch settings */
+#define CONFIG_BOOTCOMMAND		"run $modeboot"
+#define CONFIG_BOOTDELAY		3 /* -1 to Disable autoboot */
+#define CONFIG_SYS_LOAD_ADDR		0 /* default? */
+
+/* Keep L2 Cache Disabled */
+#define CONFIG_SYS_L2CACHE_OFF
+#define CONFIG_SYS_CACHELINE_SIZE	32
+
+/* Physical Memory map */
+#define CONFIG_NR_DRAM_BANKS		1
+#define PHYS_SDRAM_1			0
+#define CONFIG_SYS_TEXT_BASE		0x04000000
+
+#define CONFIG_SYS_MEMTEST_START	PHYS_SDRAM_1
+#define CONFIG_SYS_MEMTEST_END		(CONFIG_SYS_MEMTEST_START + \
+					PHYS_SDRAM_1_SIZE - (16 * 1024 * 1024))
+
+#define CONFIG_SYS_INIT_RAM_ADDR	0xFFFF0000
+#define CONFIG_SYS_INIT_RAM_SIZE	0x1000
+#define CONFIG_SYS_INIT_SP_ADDR		(CONFIG_SYS_INIT_RAM_ADDR + \
+					CONFIG_SYS_INIT_RAM_SIZE - \
+					GENERATED_GBL_DATA_SIZE)
+
+/* Enable the PL to be downloaded */
+#define CONFIG_FPGA
+#define CONFIG_FPGA_XILINX
+#define CONFIG_FPGA_ZYNQPL
+#define CONFIG_CMD_FPGA
+
+/* FIT support */
+#define CONFIG_FIT		1
+#define CONFIG_FIT_VERBOSE	1 /* enable fit_format_{error,warning}() */
+
+#define CONFIG_CMD_BOOTZ
+#undef CONFIG_BOOTM_NETBSD
+
+/* FIXME this should be removed pretty soon */
+#define XPSS_QSPI_BASEADDR		0xE000D000
+#define XPSS_NAND_BASEADDR		0xE1000000
+#define XPSS_CRTL_PARPORT_BASEADDR	0xE000E000
+// For SD card in TE0701
+#define SD_BASEADDR			0xE0100000
+// For MMC onboard chip 
+//#define SD_BASEADDR     		0xE0101000
+//	"ethaddr=00:0a:35:00:01:22\0"					\
+
 
 #undef CONFIG_EXTRA_ENV_SETTINGS
-#define CONFIG_EXTRA_ENV_SETTINGS	\
-	"ethaddr=00:0a:35:00:01:22\0"	\
-	"kernel_image=uImage\0"	\
-	"ramdisk_image=uramdisk.image.gz\0"	\
-	"devicetree_image=devicetree.dtb\0"	\
-	"bitstream_image=system.bit.bin\0"	\
-	"loadbit_addr=0x100000\0"	\
-	"kernel_size=0x500000\0"	\
-	"devicetree_size=0x20000\0"	\
-	"ramdisk_size=0x5E0000\0"	\
-	"fdt_high=0x20000000\0"	\
-	"initrd_high=0x20000000\0"	\
-	"kerel_offset=0x450000\0" \
-	"qspiboot=echo Copying Linux from QSPI flash to RAM... && " \
-		"sf probe 0 0 0 && " \
-		"sf read 0x3000000 0x450000 ${kernel_size} && " \
-		"sf read 0x2A00000 0x950000 ${devicetree_size} && " \
-		"sf read 0x2000000 0x970000 ${ramdisk_size} && " \
-		"bootm 0x3000000 0x2000000 0x2A00000\0" \
-	"sdboot=echo Copying Linux from SD to RAM... && " \
-		"mmcinfo && " \
-		"fatload mmc 0 0x3000000 ${kernel_image} && " \
-		"fatload mmc 0 0x2A00000 ${devicetree_image} && " \
-		"fatload mmc 0 0x2000000 ${ramdisk_image} && " \
-		"bootm 0x3000000 0x2000000 0x2A00000\0" \
-	"fetch=echo TFTPing Linux to RAM... && " \
-		"tftp 0x3000000 ${kernel_image} && " \
-		"tftp 0x2A00000 ${devicetree_image} && " \
-		"tftp 0x2000000 ${ramdisk_image}\0" \
-	"reflash=echo Write Linux from RAM to QSPI flash... && " \
-		"sf probe 0 0 0 && " \
-		"sf update 0x3000000 0x450000 ${kernel_size} && " \
-		"sf update 0x2A00000 0x950000 ${devicetree_size} && " \
-		"sf update 0x2000000 0x970000 ${ramdisk_size}\0" \
-	"linux=bootm 0x3000000 0x2000000 0x2A00000\0" \
-	"tftpboot=echo TFTPing Linux to RAM... && " \
-		"tftp 0x3000000 ${kernel_image} && " \
-		"tftp 0x2A00000 ${devicetree_image} && " \
-		"tftp 0x2000000 ${ramdisk_image} && " \
-		"bootm 0x3000000 0x2000000 0x2A00000\0"
-
-/*
-#define CONFIG_EXTRA_ENV_SETTINGS	\
-	"ethaddr=00:0a:35:00:01:22\0"	\
-	"kernel_image=uImage\0"	\
-	"ramdisk_image=uramdisk.image.gz\0"	\
-	"devicetree_image=devicetree.dtb\0"	\
-	"bitstream_image=system.bit.bin\0"	\
-	"kernel_size=0x500000\0"	\
-	"devicetree_size=0x20000\0"	\
-	"ramdisk_size=0x5E0000\0"	\
-	"kernel_offset=0x450000\0" \
-	"devicetree_offset=0x950000\0" \
-	"ramdisk_offset=0x970000\0" \
-	"kernel_addr=0x3000000\0"	\
-	"devicetree_addr=0x2A00000\0"	\
-	"ramdisk_addr=0x2000000\0"	\
-	"qspiboot=echo Copying Linux from QSPI flash to RAM... && " \
-		"sf probe 0 0 0 && " \
-		"sf read ${kernel_addr} ${kernel_offset} ${kernel_size} && " \
-		"sf read ${devicetree_addr} ${devicetree_offset} ${devicetree_size} && " \
-		"echo Copying ramdisk... && " \
-		"sf read ${ramdisk_addr} ${ramdisk_offset} ${ramdisk_size} && " \
-		"bootm ${kernel_addr} ${ramdisk_addr} ${devicetree_addr}\0" \
-	"sdboot=echo Copying Linux from SD to RAM... && " \
-		"mmcinfo && " \
-		"fatload mmc 0 ${kernel_addr} ${kernel_image} && " \
-		"fatload mmc 0 ${devicetree_addr} ${devicetree_image} && " \
-		"fatload mmc 0 ${ramdisk_addr} ${ramdisk_image} && " \
-		"bootm ${kernel_addr} ${ramdisk_addr} ${devicetree_addr}\0" \
-	"fetch=echo TFTPing Linux to RAM... && " \
-		"tftp ${kernel_addr} ${kernel_image} && " \
-		"tftp ${devicetree_addr} ${devicetree_image} && " \
-		"tftp ${ramdisk_addr} ${ramdisk_image}\0" \
-	"reflash_all=echo Write from RAM to SPI... && " \
-		"sf probe && " \
-		"sf update ${kernel_addr} ${kernel_offset} ${kernel_size} && " \
-		"sf update ${devicetree_addr} ${devicetree_offset} ${devicetree_size} && " \
-		"sf update ${ramdisk_addr} ${ramdisk_offset} ${ramdisk_size}\0" \
-	"reflash_dt=echo Write Devicetree from RAM to SPI... && " \
-		"sf probe && " \
-		"sf update ${devicetree_addr} ${devicetree_offset} ${devicetree_size}\0" \
-	"linux=bootm ${kernel_addr} ${ramdisk_addr} ${devicetree_addr}\0"\
-	"tftpboot=echo TFTPing Linux to RAM... && " \
-		"tftp ${kernel_addr} ${kernel_image} && " \
-		"tftp ${devicetree_addr} ${devicetree_image} && " \
-		"tftp ${ramdisk_addr} ${ramdisk_image} && " \
-		"bootm ${kernel_addr} ${ramdisk_addr} ${devicetree_addr}\0"\
-*/
+#define CONFIG_EXTRA_ENV_SETTINGS					\
+	"bitstream_image=boot.bin\0"					\
+	"bitstream_addr=0x1000000\0"					\
+	"bitstream_size=0x450000\0"					\
+	"bitstream_offset=0x000000\0"					\
+	"kernel_image=uImage\0"						\
+	"kernel_addr=0x3000000\0"					\
+	"kernel_size=0x500000\0"					\
+	"kernel_offset=0x450000\0" 					\
+	"devicetree_image=devicetree.dtb\0"				\
+	"devicetree_addr=0x2A00000\0"					\
+	"devicetree_size=0x20000\0"					\
+	"devicetree_offset=0x950000\0"					\
+	"ramdisk_image=uramdisk.image.gz\0"				\
+	"ramdisk_addr=0x2000000\0"					\
+	"ramdisk_size=0x5E0000\0"					\
+	"ramdisk_offset=0x970000\0" 					\
+	"fdt_high=0x20000000\0"						\
+	"initrd_high=0x20000000\0"					\
+	"qspiboot=echo Trenz-Electronic ${board} SC ${scver} && "			\
+		"echo Copying Linux from QSPI flash to RAM... && " 			\
+		"sf probe 0 0 0 && " 							\
+		"sf read ${kernel_addr} ${kernel_offset} ${kernel_size} && "		\
+		"sf read ${devicetree_addr} ${devicetree_offset} ${devicetree_size} && "\
+		"sf read ${ramdisk_addr} ${ramdisk_offset} ${ramdisk_size} && " 	\
+		"fdt addr ${devicetree_addr} &&"				\
+		"fdt set /amba/eth local-mac-address ${eui48} &&"		\
+		"bootm  ${kernel_addr} ${ramdisk_addr} ${devicetree_addr}\0"	\
+	"sdboot=echo Trenz-Electronic ${board}  ${scver} && "			\
+		"echo Copying Linux from SD to RAM... && " 			\
+		"mmcinfo && " 							\
+		"fatload mmc 0 ${kernel_addr} ${kernel_image} && " 		\
+		"fatload mmc 0 ${devicetree_addr} ${devicetree_image} && " 	\
+		"fatload mmc 0 ${ramdisk_addr} ${ramdisk_image} && " 		\
+		"fdt addr ${devicetree_addr} &&"				\
+		"fdt set /amba/eth local-mac-address ${eui48} &&"		\
+		"bootm ${kernel_addr} ${ramdisk_addr} ${devicetree_addr}\0" 	\
+	"tftpfetch=echo TFTPing Linux to RAM... && " 				\
+		"tftp ${kernel_addr} ${kernel_image} && " 			\
+		"tftp ${devicetree_addr} ${devicetree_image} && " 		\
+		"tftp ${ramdisk_addr} ${ramdisk_image} && " 			\
+		"fdt addr 0x950000 \0"						\
+	"sdfetch=echo Read files from SD... && " 				\
+		"mmcinfo && " 							\
+		"fatload mmc 0 ${bitstream_addr} ${bitstream_image} && " 	\
+		"fatload mmc 0 ${kernel_addr} ${kernel_image} && " 		\
+		"fatload mmc 0 ${devicetree_addr} ${devicetree_image} && " 	\
+		"fatload mmc 0 ${ramdisk_addr} ${ramdisk_image} && " 		\
+		"fdt addr ${devicetree_addr} &&"				\
+		"fdt set /amba/eth local-mac-address ${eui48}\0"		\
+	"reflash_all=echo Full reflash from Memory... && " 					\
+		"sf probe 0 0 0 && " 								\
+		"sf update ${bitstream_addr} ${bitstream_offset} ${bitstream_size} && " 	\
+		"sf update ${kernel_addr} ${kernel_offset} ${kernel_size} && " 			\
+		"sf update ${devicetree_addr} ${devicetree_offset} ${devicetree_size} && "	\
+		"sf update ${ramdisk_addr} ${ramdisk_offset} ${ramdisk_size}\0" 		\
+	"reflash_linux=echo Reflash linux from Memory... && " 					\
+		"sf probe 0 0 0 && " 								\
+		"sf update ${kernel_addr} ${kernel_offset} ${kernel_size} && " 			\
+		"sf update ${devicetree_addr} ${devicetree_offset} ${devicetree_size} && "	\
+		"sf update ${ramdisk_addr} ${ramdisk_offset} ${ramdisk_size}\0" 		\
+	"reflash_dt=echo Reflash Device Tree from Memory... && " 				\
+		"sf probe 0 0 0 && " 								\
+		"sf update ${devicetree_addr} ${devicetree_offset} ${devicetree_size}\0"	\
+	"linux=bootm  ${kernel_addr} ${ramdisk_addr} ${devicetree_addr}\0"	\
+	"linaro=bootm  ${kernel_addr} - ${devicetree_addr}\0"			\
+	"tftpboot=echo TFTPing Linux to RAM... && " 				\
+		"tftp ${kernel_addr} ${kernel_image} && " 			\
+		"tftp ${devicetree_addr} ${devicetree_image} && " 		\
+		"tftp ${ramdisk_addr} ${ramdisk_image} && " 			\
+		"bootm  ${kernel_addr} ${ramdisk_addr} ${devicetree_addr}\0"	\
+	"updatemac=fdt addr ${devicetree_addr} && "\
+		"fdt set /amba/eth local-mac-address \"${eui48}\"\0"
 #endif /* __CONFIG_ZYNQ_GIGAZEE_H */
